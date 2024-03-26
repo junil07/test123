292d8bdb1aaecbc1188e1a3d0d78072a94d4d157
chaseongjin <cktjdwls7979@naver.com>
2024-03-26 11:21:34 +0900

    BuyList(Bean, Mgr), Choice(Bean, Mgr), Explanation(Bean, Mgr), Manager(Bean, Mgr) 생성

---
+++ TestProject/src/main/java/project/ChoiceBean.java
@@ -0,0 +1,46 @@
+package project;
+
+public class ChoiceBean {
+	
+	private int choice_num, choice_question_num, choice_number, choice_filesize;
+	private String choice_content, choice_file;
+	public int getChoice_num() {
+		return choice_num;
+	}
+	public void setChoice_num(int choice_num) {
+		this.choice_num = choice_num;
+	}
+	public int getChoice_question_num() {
+		return choice_question_num;
+	}
+	public void setChoice_question_num(int choice_question_num) {
+		this.choice_question_num = choice_question_num;
+	}
+	public int getChoice_number() {
+		return choice_number;
+	}
+	public void setChoice_number(int choice_number) {
+		this.choice_number = choice_number;
+	}
+	public int getChoice_filesize() {
+		return choice_filesize;
+	}
+	public void setChoice_filesize(int choice_filesize) {
+		this.choice_filesize = choice_filesize;
+	}
+	public String getChoice_content() {
+		return choice_content;
+	}
+	public void setChoice_content(String choice_content) {
+		this.choice_content = choice_content;
+	}
+	public String getChoice_file() {
+		return choice_file;
+	}
+	public void setChoice_file(String choice_file) {
+		this.choice_file = choice_file;
+	}
+	
+	
+	
+}
---
+++ TestProject/src/main/java/project/ExplanationBean.java
@@ -0,0 +1,46 @@
+package project;
+
+public class ExplanationBean {
+	
+	private int explanation_num, explanation_paypost_num, explanation_question_num, explanation_filesize;
+	private String explanation_content, explanation_file;
+	public int getExplanation_num() {
+		return explanation_num;
+	}
+	public void setExplanation_num(int explanation_num) {
+		this.explanation_num = explanation_num;
+	}
+	public int getExplanation_paypost_num() {
+		return explanation_paypost_num;
+	}
+	public void setExplanation_paypost_num(int explanation_paypost_num) {
+		this.explanation_paypost_num = explanation_paypost_num;
+	}
+	public int getExplanation_question_num() {
+		return explanation_question_num;
+	}
+	public void setExplanation_question_num(int explanation_question_num) {
+		this.explanation_question_num = explanation_question_num;
+	}
+	public int getExplanation_filesize() {
+		return explanation_filesize;
+	}
+	public void setExplanation_filesize(int explanation_filesize) {
+		this.explanation_filesize = explanation_filesize;
+	}
+	public String getExplanation_content() {
+		return explanation_content;
+	}
+	public void setExplanation_content(String explanation_content) {
+		this.explanation_content = explanation_content;
+	}
+	public String getExplanation_file() {
+		return explanation_file;
+	}
+	public void setExplanation_file(String explanation_file) {
+		this.explanation_file = explanation_file;
+	}
+	
+	
+	
+}
